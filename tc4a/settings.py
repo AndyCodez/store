720c8267ce2f3ebf64be02e95ad922f304341e80
Andrew Owino <andystudy4@gmail.com>
2024-06-27 11:15:25 +0300

    Configure for vercel

--- tc4a/settings.py
+++ tc4a/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-85p$k^zi0$1ui&8$04f7@9zm_#o%%@176%5flaji0q3nh=%8_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -143,6 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# The directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
